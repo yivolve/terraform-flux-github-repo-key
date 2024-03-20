e09994228777327b101767b3b77c8d5f1683d732
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-03-19 21:51:27 -0600

    Adding initial files

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "tls_private_key" "this" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+resource "github_repository_deploy_key" "this" {
+  title      = var.title
+  repository = var.repository
+  key        = tls_private_key.this.public_key_openssh
+  read_only  = "false"
+}
+
+resource "aws_secretsmanager_secret" "this" {
+  name                    = var.title
+  description             = var.description
+  recovery_window_in_days = 0
+  tags                    = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "flux_secret_version" {
+  secret_id     = aws_secretsmanager_secret.this.id
+  secret_string = jsonencode(tls_private_key.this.private_key_pem)
+  lifecycle {
+    ignore_changes = [secret_string, ]
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "secret_name" {
+  value =   aws_secretsmanager_secret.flux_secret.name
+  sensitive = true
+}
